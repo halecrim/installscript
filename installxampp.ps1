4a7d985548fbcf40b6050caa6660297ba95b5fe1
halecrim <37438973+halecrim@users.noreply.github.com>
2021-02-22 08:01:27 -0800

    Update installxampp.ps1

--- installxampp.ps1
+++ installxampp.ps1
@@ -1,1 +1,1 @@
-Start-Process -Wait -FilePath "https://www.apachefriends.org/xampp-files/8.0.2/xampp-windows-x64-8.0.2-0-VS16-installer.exe" -ArgumentList '/S','/v','/qn' -passthru
+Start-Process -Wait -FilePath "C:\Users\User\Downloads\xampp-windows-x64-8.0.2-0-VS16-installer.exe" -ArgumentList '/S','/v','/qn' -passthru
