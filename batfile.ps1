ec23673ec2f12e2ae836a78d97440c7770a8faf6
halecrim <37438973+halecrim@users.noreply.github.com>
2021-04-22 14:28:28 -0700

    Update batfile.ps1

--- batfile.ps1
+++ batfile.ps1
@@ -1,2 +1,1 @@
-Start-Process 'cmd' -ArgumentList  '/c \\DEMOLATAM-DEPLO\Users\Admin\Downloads\Scrit de teste 123\safe a file.bat && exit'
-Start-Process 'cmd'  -ArgumentList '/c calc && exit'
+Start-Process 'cmd' -ArgumentList  '/c "\\DEMOLATAM-DEPLO\Users\Admin\Downloads\Scrit de teste 123\safe a file.bat" && exit'
