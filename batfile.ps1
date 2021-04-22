78ab9801b25982d7f580ad01ea54c7a5702f398b
halecrim <37438973+halecrim@users.noreply.github.com>
2021-04-22 14:31:10 -0700

    Update batfile.ps1

--- batfile.ps1
+++ batfile.ps1
@@ -1,1 +1,1 @@
-Start-Process 'cmd' -ArgumentList  '/c "\\DEMOLATAM-DEPLO\Users\Admin\Downloads\Scrit de teste 123\safe a file.bat" && exit'
+Start-Process 'cmd.exe' -ArgumentList  '/c "\\DEMOLATAM-DEPLO\Users\Admin\Downloads\Scrit de teste 123\safe a file.bat" && exit'
